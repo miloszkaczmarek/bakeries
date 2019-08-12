50f1654777d795035a50ccb65a352b6530b3cce2
Miłosz Kaczmarek <miloszkaczmarek@hotmail.com>
2019-08-12 10:45:57 +0200

    Added bakeries from API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
 
+  const [bakeries, setBakeries] = useState([]);
+
+  useEffect(() => {
+    fetch("https://nominatim.openstreetmap.org/?format=json&addressdetails=1&q=bakery+in+warsaw&format=json&limit=20")
+      .then(res => res.json())
+      .then(json => setBakeries(json));
+  }, []);
+
+  console.log(bakeries[4]);
+
   return (
-    <h1>Bakeries</h1>
+    <div>
+      <h1>Bakeries</h1>
+      <ul>
+        {bakeries.map((bakery, index) => {
+
+            if(bakery.address.bakery != null && index < 11){
+              return <div>
+                <h3>{bakery.address.bakery}</h3>
+                <p>{bakery.address.road} {bakery.address.house_number}, {bakery.address.city}</p>
+              </div>
+
+            }
+
+          })}
+          </ul>
+    </div>
   );
 }
 
